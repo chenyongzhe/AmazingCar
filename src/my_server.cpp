b805c6419e3c3ae62796704bf020cc5a3ddc3dae
guojingming <1043840980@qq.com>
2018-09-09 16:39:22 +0800

    latest

--- src/my_server.cpp
+++ src/my_server.cpp
@@ -14,19 +14,26 @@ using namespace std;
 
 extern struct CYCarPoint;
 
+int ordered_cmd_count = 0;
+int recieved_cmd_count = 0;
+int cmd_flag[100] = {0};
+ros::Publisher * p_tars_pub = nullptr;
+vector<CYCarPoint> tars;
+
 serial::Serial my_serial("/dev/ttyUSB0", 115200, serial::Timeout::simpleTimeout(1000));
 
 void callback_state(const amazing_car::my_car_state state);
 void SendCarData(float x,float y,float angle,int state);
 void process_cmd(const ros::Publisher & cmd_pub, string cmd);
-void init_tars_queue(string cmd);
-void add_tar_to_queue(std::queue<CYCarPoint> & queue, string cmd);
+void init_tars_queue(vector<CYCarPoint> & tar_vector, string cmd);
+void add_tar_to_queue(vector<CYCarPoint> & tar_vector, string cmd);
 
 int main(int argc, char ** argv){
 	ros::init(argc, argv, "my_server");
 	ros::NodeHandle n;
 	ros::Publisher cmd_pub = n.advertise<amazing_car::my_server_cmd>("server_cmd", 1000);
 	ros::Publisher tars_pub = n.advertise<amazing_car::my_checkpoints>("my_checkpoints", 1000);
+	p_tars_pub = &tars_pub;
 	ros::Subscriber car_state_sub = n.subscribe("my_car_state", 1000, callback_state);
 	ros::Rate rate(20);
 	//auth
@@ -69,7 +76,7 @@ void process_cmd(const ros::Publisher & cmd_pub, string cmd){
 
 	if(cmd.find("#GNSS_OPEN") == 0){
 		//get serial number
-		cmd = cmd.substr(cmd.find("N_") + 2);
+		cmd = cmd.substr(cmd.find("OPEN_") + 5);
 		cmd = cmd.substr(0, cmd.find("$"));
 		int serial_num = atoi(cmd.c_str());
 		//write file
@@ -81,7 +88,7 @@ void process_cmd(const ros::Publisher & cmd_pub, string cmd){
 
 	if(cmd.find("#CONTROLLER_OPEN") == 0){
 		//get serial number
-		cmd = cmd.substr(cmd.find("N_") + 2);
+		cmd = cmd.substr(cmd.find("OPEN_") + 5);
 		cmd = cmd.substr(0, cmd.find("$"));
 		int serial_num = atoi(cmd.c_str());
 		//write file
@@ -104,11 +111,11 @@ void process_cmd(const ros::Publisher & cmd_pub, string cmd){
 	}
 
 	if(cmd.find("#TARS_COUNT") == 0){
-		init_tars_queue(cmd);
+		init_tars_queue(tars, cmd);
 	}
 
 	if(cmd.find("#TARS_") == 0){
-		add_tar_to_queue(cmd);
+		add_tar_to_queue(tars, cmd);
 	}
 
 	amazing_car::my_server_cmd ros_cmd;
@@ -158,10 +165,58 @@ void callback_state(const amazing_car::my_car_state state){
 	SendCarData(state.x, state.y, state.angle, state.state);
 }
 
-void init_tars_queue(string cmd){
-
+void init_tars_queue(vector<CYCarPoint> & tar_vector, string cmd){
+	cmd = cmd.substr(cmd.find("COUNT_") + 6);
+	cmd = cmd.substr(0, cmd.find("$"));
+	ordered_cmd_count = atoi(cmd.c_str());
+	memset(cmd_flag, 0, 100);
+	tar_vector.clear();
+	for(int i = 0;i < ordered_cmd_count;i++){
+		tar_vector.push_back(CYCarPoint(0, 0));
+	}
 }
 
-void add_tar_to_queue(queue<CYCarPoint> & queue, string cmd){
+void add_tar_to_queue(vector<CYCarPoint> & tar_vector, string cmd){
+	if(ordered_cmd_count == 0){
+		return;
+	}
+	//"#TAR_Np1_Xp2_Yp3$"
+	int _N_index = cmd.find("_N");
+	int _X_index = cmd.find("_X");
+	int _Y_index = cmd.find("_Y");
+	int _N = atoi(cmd.substr(_N_index + 2, _X_index - _N_index).c_str());
+	int _X = atoi(cmd.substr(_X_index + 2, _Y_index - _X_index).c_str());
+	int _Y = atoi(cmd.substr(_Y_index + 2, cmd.find("$") - _Y_index).c_str());
 	
+	if(_N >= ordered_cmd_count){
+		return;
+	}
+
+	if(cmd_flag[_N] == 0){
+		tar_vector[_N].x = _X;
+		tar_vector[_N].y = _Y;
+		cmd_flag[_N] = 1;
+		recieved_cmd_count++;
+		if(recieved_cmd_count == ordered_cmd_count){
+			amazing::my_checkpoints checkpoints;
+			checkpoints.count = recieved_cmd_count;
+			checkpoints.X.resize(recieved_cmd_count);
+			checkpoints.Y.resize(recieved_cmd_count);
+			for(int i = 0;i<recieved_cmd_count;i++){
+				checkpoints.X[i] = tar_vector[i].x;
+				checkpoints.Y[i] = tar_vector[i].y;
+			}
+			p_tars_pub->publish(checkpoints);
+			ordered_cmd_count = 0;
+			recieved_cmd_count = 0;
+			memset(cmd_flag, 0, 100);
+			tar_vector.clear();
+		}
+	}else if(cmd_flag[_N] == 1){
+		//wrong
+		ordered_cmd_count = 0;
+		recieved_cmd_count = 0;
+		memset(cmd_flag, 0, 100);
+		tar_vector.clear();
+	}
 }
